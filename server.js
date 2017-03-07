25ed93800a1b42bc6e52568264ac20b05047b3e6
Keli Fine <kelfin373@gmail.com>
2017-03-07 21:38:37 +0000

    first answer

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var moment = require('moment');
+moment().format();
+var chrono = require('chrono-node');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var object;
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+function dateChecker (date) {
+    if (isNaN(date)===false) {
+        object = {
+           unix: date,
+           natural: moment.unix(date).format("MMM Do, YYYY")
+       }; 
+    }
+    else if (Boolean(chrono.parseDate(date))!== false ) {
+          object = {
+        unix: Date.parse(chrono.parseDate(date)),
+        natural: moment(chrono.parseDate(date)).format("MMM Do, YYYY")
+    }
+}
+else {
+    object = {
+        unix: null,
+        natural: null,
+    };
+}
+}
+
+app.get('*', function(req, res) {
+    var date = req.path.substr(1); 
+    dateChecker(date);
+    res.send(object);
 });
+app.listen(8080);
